2625674d0c16c04b5a0fa0dedde7f272a6d9a463
Puja1156 <pujubudhavale2005@gmail.com>
2025-02-01 20:10:23 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which OSI layer ensure reliable data transfer?",
+        answer: "D.Transport",
+        options: [
+            "A.Data Link",
+            "B.Network",
+            "C.Application",
+            "D.Transport",
+        ]
+    },
+    {
+        numb: 2,
+        question: "How do you create a new Django project?",
+        answer: "B.django startproject project_name",
+        options: [
+            "A.django new project_name",
+            "B.django startproject project_name",
+            "C.django create project_name",
+            "D.django init project_name",
+        ]
+    },
+    {
+        numb: 3,
+        question: "How can you include a template in another template in Django?",
+        answer: "B.Using the {% include %} tag",
+        options: [
+            "A.Using the {% load %} tag",
+            "B.Using the {% include %} tag",
+            "C.Using the {% extends %} tag",
+            "D.Using the {% block %} tag",
+        ]
+    },
+    {
+        numb: 4,
+        question: "How can you include static files like CSS and JavaScript in Django?",
+        answer: "A.Using the {%load static%} template tag",
+        options: [
+            "A.Using the {%load static%} template tag",
+            "B.By placing them in the template directory",
+            "C.By adding them to the models.py file",
+            "D.Including them directly in the views.py file",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which Django tag is used to include a template inside the current template?",
+        answer: "D.include",
+        options: [
+            "A.allow",
+            "B.insert",
+            "C.import",
+            "D.include",
+        ]
+    },
+];
